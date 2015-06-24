bea95fd513d87013376337b670f8d8561fcd6558
Chris Cowley <chris@chriscowley.me.uk>
2015-06-24 14:50:09 +0000

    Use a template instead of file - might be useful in the future

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,13 @@
 class puppetclean {
-  file { '/usr/local/bin/puppet-reports-staker':
-    source => 'puppet://modules/puppetclean/puppet-report-stalker',
+  file { '/usr/local/bin/puppet-report-stalker':
+    source => template('puppetclean/puppet-report-stalker.erb')
     before => Cron['clean reports'],
     mode   => 700,
     owner  => 'root',
     group  => 'root',
   }
   cron { 'clean reports':
-    command => '/usr/local/bin/puppet-reports-staker',
+    command => '/usr/local/bin/puppet-report-stalker',
     user    => 'root',
     hour    => 4,
     minute  => 2,
