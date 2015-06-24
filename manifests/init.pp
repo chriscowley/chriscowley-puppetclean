acbc4bd221918e9c74bcfcc725ec832f89f09126
Chris Cowley <chris@chriscowley.me.uk>
2015-06-24 16:53:04 +0200

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,6 @@
 class puppetclean {
   file { '/usr/local/bin/puppet-report-stalker':
-    source => template('puppetclean/puppet-report-stalker.erb')
+    source => template('puppetclean/puppet-report-stalker.erb'),
     before => Cron['clean reports'],
     mode   => 700,
     owner  => 'root',
